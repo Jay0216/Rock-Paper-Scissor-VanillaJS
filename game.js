1d2ad8c1a9a7f2809f4788bb1fb794090f272f1f
Navod Jayasundara <navoddulanjana14@gmail.com>
2024-02-05 21:22:36 +0530

    All Files are added

---
+++ game.js
@@ -0,0 +1,28 @@
+console.log("Vanilla Javascript Rock Paper Scissor Game Console")
+
+let button = document.getElementById("click")
+let text = document.getElementById("text")
+
+
+
+//add images to change the hand type
+const click = () => {
+
+    console.log("Clicked Game Started")
+    text.style.color = "Blue"
+}
+
+button.addEventListener("click", click)
+
+
+
+
+const hand_change = () => {
+
+    console.log("Hand Change")
+}
+
+
+
+
+
